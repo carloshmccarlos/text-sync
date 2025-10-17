d099648f86ef31d9efc28a154c9d3fe44ab3d8f1
carloshmccarlos <carloshmccarlos@gmail.com>
2025-10-17 15:07:15 +0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,17 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import react from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
-		tsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
+		tsConfigPaths(),
 		tailwindcss(),
-		tanstackStart({
-			target: "cloudflare-module",
-
-			// https://github.com/TanStack/router/discussions/2863#discussioncomment-13713677
-			customViteReactPlugin: true,
-
-			tsr: {
-				quoteStyle: "double",
-				semicolons: true,
-			},
-
-			// https://tanstack.com/start/latest/docs/framework/react/hosting#deployment
-			// target: "node-server",
-		}),
-		react({
-			// https://react.dev/learn/react-compiler
-			babel: {
-				plugins: [
-					[
-						"babel-plugin-react-compiler",
-						{
-							target: "19",
-						},
-					],
-				],
-			},
-		}),
+		tanstackStart({}),
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		tanstackStart(),
+		viteReact(),
 	],
 });
